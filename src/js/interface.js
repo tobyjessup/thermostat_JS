dd39e6061b03da9ddee8a7c7de3fb795c85ffacd
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-05 23:10:54 +0100

    added css and further dom funcationality

--- src/js/interface.js
+++ src/js/interface.js
@@ -1,18 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
+  function updateTemperature() {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#temperature').className = thermostat.energyUsage();
+  };
+
   const thermostat = new Thermostat();
-  console.log(thermostat)
-  document.querySelector('#temperature').innerText = thermostat.temperature
+  updateTemperature()
 
   document.querySelector("#temperature-up").addEventListener('click', function(){
-    console.log(thermostat)
     thermostat.up();
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    updateTemperature()
   })
 
   document.querySelector('#temperature-down').addEventListener('click', function() {
     thermostat.down();
-    document.querySelector('#temperature').innerText = thermostat.temperature;
+    updateTemperature()
   })
 
+  document.querySelector('#temperature-reset').addEventListener('click', function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  document.querySelector('#powersaving-on').addEventListener('click', function() {
+    thermostat.switchPowerSavingModeOn();
+    document.querySelector('#power-saving-status').innerText = 'on';
+    updateTemperature();
+  })
+
+  document.querySelector('#powersaving-off').addEventListener('click', function() {
+    thermostat.switchPowerSavingModeOff();
+    document.querySelector('#power-saving-status').innerText = 'off';
+    updateTemperature();
+  })
 });
 
